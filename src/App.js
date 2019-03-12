5d42998dacc40ee00deea45ed57697f4ff3ae1f8
olakunle <oraclee10gee@gmail.com>
2019-03-12 10:16:51 +0100

    made some changes to my landing page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import LandingPage from "./LandingPage";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import '../node_modules/font-awesome/css/font-awesome.min.css';
 
 
 class App extends Component {
--- src/LandingPage.js
+++ src/LandingPage.js
@@ -3,24 +3,22 @@ import React, { Component } from 'react';
 export default class LandingPage extends Component {
   render() {
     return (       
-      <div>
-            <div class="card">
-                <div class="card-header">
-                <ul class="nav nav-tabs card-header-tabs">
-                <li class="nav-item"><a class="nav-link active" href="#">Time off Management</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Pricing</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Sign In</a></li>
-                </ul>
-               </div>
-            <div class="card-body text-center">
-            <h1 class="text-center card-title">TimeOff.Management</h1>
-            <p class="card-text text-center"  >Open source, simple yet powerful absence management software for small and medium size business.</p>
-            <p class='text-center'>Endorsed by&nbsp;<a href="https://bootstrapstudio.io/releases/app/4.1.2/#">hundreds&nbsp;</a>of software developers</p>
-            <button class="btn btn-success text-center"
-                type="button">Sign Up</button>
+      <div>     
+                <nav class="navbar">
+                  <ul class="nav nav-tabs card-header-tabs">
+                  <li class="nav-item"><a class="nav-link" href="#">Time off Management</a></li>
+                  <button id="sign in" class="btn btn-success">Sign in</button>
+                  </ul>
+               </nav>
+                <div class="card card-body text-center">
+                  <h1 class="text-center card-title">TimeOff.Management</h1>
+                  <p class="card-text text-center"  >Open source, simple yet powerful absence management software for small and medium size business.</p>
+                  <p class='text-center'>Endorsed by&nbsp;<a href="https://bootstrapstudio.io/releases/app/4.1.2/#">hundreds&nbsp;</a>of software developers</p>
+                  <button class="btn btn-success">Sign Up</button>
                 </div>
-    </div>
-      </div>
+
+        </div>
+    
     )
   }
 }
